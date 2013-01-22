5fce36d472124a01869cecd9f7022d970541ea33
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-01-22 00:27:38 -0500

    Cleaning up pages, placing in all content areas

--- js/ookb.js
+++ js/ookb.js
@@ -18,11 +18,17 @@
   // Some Custom JS for the site
   // scroll to content when you click on the logo. - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
   $(document).ready(function() {
-    $("section.toggle article ").hide();
+    $("section.toggle > article ").hide();
     });
 	
-  $("section.toggle").click(function () {
-    $(this).children("article").toggle("slow");
+  $("section.toggle h2").click(function () {
+    $(this).parent().children("article").toggle("slow");
     });    
+
+/*
+  $("section.toggle").click(function () {
+    $(this).children("article").show("slow");
+    });    
+*/
 
 }(window.jQuery);
