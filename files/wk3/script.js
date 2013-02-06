9131e97af2d0af375b6de61b81a8a77702bc59e7
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-02-06 11:17:11 -0500

    adding some positioing test files

---
+++ files/wk3/script.js
@@ -0,0 +1,15 @@
+  $(".red").click(function () {
+    $("body").removeClass("green white yellow blue").addClass("red");
+    });    
+
+  $(".green").click(function () {
+    $("body").removeClass("red white yellow blue").addClass("green");
+    });    
+
+  $(".blue").click(function () {
+    $("body").removeClass("red green white yellow").addClass("blue");
+    });    
+
+  $(".yellow").click(function () {
+    $("body").removeClass("red white blue green").addClass("yellow");
+    });
